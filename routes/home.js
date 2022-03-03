5581e4552224d65ecc713e47c42ff884a51eaa87
Ajil Pappachan <ajilpappachan@gmail.com>
2022-03-03 20:32:12 +0530

    Added upload feature

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const multer = require("multer");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, process.env.DESTINATION);
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", (req, res) => {
 	res.render("upload");
@@ -8,6 +23,10 @@ router.get("/", (req, res) => {
 
 router.get("/files", (req, res) => {
 	res.render("files");
+});
+
+router.post("/upload", upload.array("files"), (req, res) => {
+	res.send("Uploaded");
 });
 
 module.exports = router;
