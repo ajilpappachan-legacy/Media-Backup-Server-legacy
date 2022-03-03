0a3431ee06e32dd1b9bcad57cce7cd2c5f2f0dcd
Ajil Pappachan <ajilpappachan@gmail.com>
2022-03-03 19:41:51 +0530

    Hello world server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
+
+const homeRouter = require("./routes/home");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", homeRouter);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log("listening on port " + port);
+});
